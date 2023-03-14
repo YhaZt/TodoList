408c1a0753ec578bf1c0a21030da106dd7b77017
Carpel Sweet Dreams B. Lanto <69719331+YhaZt@users.noreply.github.com>
2023-03-14 10:38:50 +0800

    test
    test

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+// carpel
     /**
      * The attributes that are mass assignable.
      *
