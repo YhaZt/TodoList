6b717e3f2f3af190f5f637ad169a90481113a55e
EricJohnMartinez <ericjohn121709@gmail.com>
2023-03-14 14:42:54 +0800

    test

--- app/database/migrations/2023_03_14_032812_create_todo_table.php
+++ app/database/migrations/2023_03_14_032812_create_todo_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->boolean('status');
+            $table->boolean('status') ->default(0);
         });
     }
 
