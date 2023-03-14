8ba5d66afeb32a10aced79c5cf7a539359c634e4
Carpel Sweet Dreams B. Lanto <69719331+YhaZt@users.noreply.github.com>
2023-03-14 14:23:51 +0800

    front
    vue

---
+++ Todo/src/components/Todo.vue
@@ -0,0 +1,55 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+<div>
+  <h1 class = "h1">TODO List</h1>
+  <br>
+  <table style="width:100%">
+    <thead>
+    <tr>
+      <td>Title</td>
+      <td>Description</td>
+      <td>Status</td>
+      <td>Action</td>
+    </tr>
+  </thead>
+  <tbody>
+
+<tr>
+  <td>{{Title}}</td>
+  <td>{{Description}}</td>
+  <td>{{Status}}</td>
+  <td>{{Action}}</td>
+</tr>
+
+  </tbody>
+  </table>
+</div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios.get('http:127.0.0.1:8080/api/index')
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+};
+</script>
